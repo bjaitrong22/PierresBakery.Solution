dd760d9d345314a0280bc921b99f634f976bebb7
Bai Jaitrong <bjaitrong@comcast.net>
2022-11-26 13:17:53 -0700

    Bread Class successfully creates a Bread object.

--- PierresBakery.Tests/ModelTests/BreadTests.cs
+++ PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -7,5 +7,12 @@ namespace PierresBakery.TestTools
   public class BreadTests
   {
     
+   [TestMethod]
+   public void BreadConstructor_CreatesInstanceOfBread_Bread()
+   {
+    Bread newBread = new Bread();
+    Assert.AreEqual(typeof(Bread), newBread.GetType()); 
+   } 
+
   }
 }
