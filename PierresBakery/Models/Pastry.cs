05525df968b123b8689df398ea857b01395ffa9e
Bai Jaitrong <bjaitrong@comcast.net>
2022-11-26 19:33:25 -0700

    Add automatic setter for NumberOfPastry().

--- PierresBakery/Models/Pastry.cs
+++ PierresBakery/Models/Pastry.cs
@@ -3,7 +3,7 @@ namespace PierresBakery
   public class Pastry
   {
     public string TypeOfPastry{get; set; }
-    public int NumberOfPastry{get; }
+    public int NumberOfPastry{get; set; }
 
     //constructor
     public Pastry(string pastryType, int quantity)
