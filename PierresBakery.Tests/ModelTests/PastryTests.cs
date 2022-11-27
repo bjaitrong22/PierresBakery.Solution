9b8bc85f621513217b232aad617cacc7f16e63cb
Bai Jaitrong <bjaitrong@comcast.net>
2022-11-26 19:00:20 -0700

    Add Red workflow for TypeOfPastry getter.

--- PierresBakery.Tests/ModelTests/PastryTests.cs
+++ PierresBakery.Tests/ModelTests/PastryTests.cs
@@ -15,5 +15,20 @@ namespace PierresBakery.TestTools
       //Assert
       Assert.AreEqual(typeof(Pastry),newPastry.GetType());
     }
+
+    [TestMethod]
+   public void GetTypeOfPastry_ReturnsTypeOfPastry_String()
+   {
+    //Arrange
+    string type = "Flaky";
+    int quantity = 1;
+    Pastry newPastry = new Pastry(type,quantity);
+
+    //Act
+    string PastryType = newPastry.TypeOfPastry;
+
+    //Assert
+    Assert.AreEqual(type,PastryType);
+   }
   }
 }
